8a9db6ff75e1022a5f9efb4b4c7d0181c0ec1b55
Rahim Rexha <143944964+Rrahimr@users.noreply.github.com>
2023-12-26 10:36:07 +0100

    updated snow*
    updated snow*

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function(){
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
-    script.onload = function () {
+    script.onload = function(){
         particlesJS("snow", {
             "particles": {
                 "number": {
